b10194e5be5f17aed91782f8142a0af27f4667e6
Tanzim <tanzimhossain2@gmail.com>
2024-05-02 19:08:06 +0600

    .

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export const metadata = {
   openGraph: {
     images: [
       {
-        url: `${process.env.NEXT_PUBLIC_SITE_URL}/api/og`,
+        url: `/api/og`,
         width: 1200,
         height: 630,
         alt: "Khana Khazana"
