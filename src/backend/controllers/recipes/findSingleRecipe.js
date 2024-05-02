9f434b9e99f17512e7461963e899355cf1562403
Tanzim <tanzimhossain2@gmail.com>
2024-05-02 15:28:16 +0600

    Fix: Single Page Data fetch

--- src/backend/controllers/recipes/findSingleRecipe.js
+++ src/backend/controllers/recipes/findSingleRecipe.js
@@ -1,7 +1,9 @@
+import { dbConnect } from "@/backend/db/connectDb";
 import { recipeModel } from "@/backend/model/Recipe";
 import { modifyData } from "@/utils/data-utils";
 
 export const findSingleRecipe = async (id) => {
+    await dbConnect();
     try {
         const recipe = await recipeModel.findById(id);
         if (recipe) {
--- src/backend/controllers/recipes/updateFavorite.js
+++ src/backend/controllers/recipes/updateFavorite.js
@@ -1,15 +1,5 @@
 const { userModel } = require("@/backend/model/User");
 
-// {
-//     _id: new ObjectId('663261a47d3b2dc5e2053011'),
-//     firstName: 'sddd',
-//     lastName: 'sddd',
-//     email: 'tanzim.pubg6@gmail.com',
-//     password: '$2a$10$HRMlub4wWre.C52Sz9oBC.2R67oWrAFHg.Bj5K0lbWthNsJmlmUdi',
-//     favourites: [],
-//     __v: 0
-//   }
-
 export const updateFavorite = async (recipeId, authId) => {
    
     try {
